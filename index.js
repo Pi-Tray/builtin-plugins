51db8c68ddee456c4afa36176adbdcc117984189
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-08-09 21:09:21 +0100

    indexing

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    edit_grid_json: require("./edit_grid_json").default,
+    open_app: require("./open_app").default,
+    open_appdata: require("./open_appdata").default,
+    open_path: require("./open_path").default,
+    run_command: require("./run_command").default
+}
